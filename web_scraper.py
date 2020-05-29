aaec9e1c4c0a31807f1a03a483ddc6127cd43b59
jacob-armiger <armigersemail@gmail.com>
2020-05-29 01:52:34 -0400

    First Working Version
    Version 1.0 is able to web scrape a comic book reading order and write it to a text file in CSV format.

---
+++ web_scraper.py
@@ -0,0 +1,53 @@
+#! python3
+import webbrowser, sys, requests, bs4, lxml, csv
+
+
+
+# Gets url of reading order page and saves to page variable
+REQUEST_URL = 'https://comicbookreadingorders.com/dc/characters/batman-reading-order/'
+page = requests.get(REQUEST_URL)
+
+# Parse HTML to find all <p> elements
+soup = bs4.BeautifulSoup(page.text, 'lxml')
+soup.prettify()
+text = soup.find_all('p')
+
+
+
+# Creates/opens text file to write to
+file = open("reading_order.txt", 'r+')
+
+# Loops through every <p> element and writes text to file. Also appends comma to
+# the end of each text element
+for t in text:
+    file.write(t.text)
+    file.write(", ")
+
+# Stores each line of file into comic_list
+comic_list = []
+while True:
+    line = file.readline()
+    comic_list.append(line)
+    if not line: break
+
+# Close File So we can overwrite later
+file.close()
+
+
+
+# Replaces new line character with a comma. This is needed to correctly
+# format file to csv
+for index in range(len(comic_list)):
+    comic_list[index] = comic_list[index].replace('\n',',')
+
+# Joins comic_list into one string, each entry separated by a comma
+csv_formatted_string = ''.join(comic_list)
+
+# Opens same file to overwrite with newly csv formatted data
+file = open("reading_order.txt", 'r+')
+
+# Rewrites file with csv string
+file.write(csv_formatted_string)
+print(csv_formatted_string)
+# Close File
+file.close()
