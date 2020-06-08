87c0497f1f1d3a300d82974949793f496f922ffb
jacob-armiger <armigersemail@gmail.com>
2020-06-08 18:03:27 -0400

    Refactor: separate each task into functions

--- web_scraper.py
+++ web_scraper.py
@@ -1,58 +1,86 @@
 #! python3
 import requests, bs4, lxml
 
-# Gets url of reading order page and saves to page variable
-REQUEST_URL = 'https://comicbookreadingorders.com/dc/characters/batman-reading-order/'
-page = requests.get(REQUEST_URL)
+def scrape(url):
+    # Gets url of reading order page and saves to page variable
+    page = requests.get(url)
 
-# Parse HTML to find all <p> elements
-soup = bs4.BeautifulSoup(page.text, 'lxml')
-soup.prettify()
-text = soup.find_all('p')
+    # Parse HTML to find all <p> elements
+    soup = bs4.BeautifulSoup(page.text, 'lxml')
+    soup.prettify()
+    text = soup.find_all('p')
 
+    return text
 
+def create_list(text):
+    # This function writes all the data scraped from URL onto a text file. Then,
+    # it stores each line in the text file into a list.
 
-# Creates/opens text file to write to
-file = open("reading_list.txt", 'w')
+    # Creates/opens text file to write comic book list to
+    file = open("reading_list.txt", 'w')
 
-# Loops through every <p> element and writes text to file. Also appends comma to
-# the end of each text element
-for t in text:
-    file.write(t.text)
-    file.write(", ")
-
-# Close File
-file.close()
-
-
-# Creates/opens text file to write to
-file = open("reading_list.txt", 'r')
-
-# Stores each line of file into comic_list
-comic_list = []
-while True:
-    line = file.readline()
-    comic_list.append(line)
-    if not line: break
-
-# Close File
-file.close()
+    # Loops through every <p> element and writes text to file. Also appends comma to
+    # the end of each text element
+    for t in text:
+        file.write(t.text)
+        file.write(", ")
+    # Close File
+    file.close()
 
 
+    # Opens text file to read from
+    file = open("reading_list.txt", 'r')
 
-# Replaces new line character with a comma. This is needed to correctly
-# format file to csv
-for index in range(len(comic_list)):
-    comic_list[index] = comic_list[index].replace('\n',',')
+    # Stores each line of file into comic_list
+    comic_list = []
+    while True:
+        line = file.readline()
+        comic_list.append(line)
+        if not line: break
+    # Close File
+    file.close()
 
-# Joins comic_list into one string, each entry separated by a comma
-csv_formatted_string = ''.join(comic_list)
+    return comic_list
 
-# Opens same file to overwrite with newly csv formatted data
-file = open("reading_list.txt", 'w')
+def clean(comic_list):
+    # This function cleans up comic_list data and then overwrites reading_list.txt
+    # with cleaned data.
 
-# Rewrites file with csv string
-file.write(csv_formatted_string)
+    # Replaces new line character with a comma. This is needed to correctly
+    # format all data entries to csv
+    for index in range(len(comic_list)):
+        comic_list[index] = comic_list[index].replace('\n',',')
 
-# Close File
-file.close()
+    # Joins comic_list into one string, each entry separated by a comma
+    csv_formatted_string = ''.join(comic_list)
+
+    # Opens file to overwrite with newly csv formatted data
+    file = open("reading_list.txt", 'w')
+
+    # Rewrites file with csv string
+    file.write(csv_formatted_string)
+
+    # Close File
+    file.close()
+
+def main():
+    # URL to be scraped
+    REQUEST_URL = 'https://comicbookreadingorders.com/dc/characters/batman-reading-order/'
+
+    # Gets comic book titles listed on URL
+    text = scrape(REQUEST_URL)
+
+    # Creates a list from the comic book titles scraped from URL
+    comic_list = create_list(text)
+
+    # Overwrites reading_list text file with relevant and better formatted entries
+    clean(comic_list)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
